70b4d90756a5924ca0dfb9961802f08d4dabfae8
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2023-04-08 16:08:07 +0200

    getting data from api

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,12 +1,24 @@
 import React, { useState, useEffect } from 'react'
 import { Box, Button, Stack, TextField, Typography } from '@mui/material'
 
+import { exerciseOptions, fetchData } from '../utils/fetchData'
+
 const SearchExercises = () => {
   const [search, setSearch] = useState('')
+  const [exercises, setExercises] = useState([])
 
   const handleSearch = async () => {
     if(search) {
-      //const exercisesData = await fetchData()
+      const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions)
+
+      const searchedExercises = exercisesData.filter(exercise => exercise.name.toLowerCase().includes(search)
+      || exercise.bodyPart.toLowerCase().includes(search)
+      || exercise.equipment.toLowerCase().includes(search)
+      || exercise.target.toLowerCase().includes(search)
+      )
+
+      setSearch('')
+      setExercises(searchedExercises)
     }
   }
 
